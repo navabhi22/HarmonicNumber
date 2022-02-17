0a451ee79747cd0372828c182d1b79e065da5f92
navabhi22 <navabhi22@gmail.com>
2022-02-17 18:10:23 +0530

    adding class or logic to get nth sum Harmonic series

---
+++ HarmonicNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber
+{
+    public class HarmonicNumber
+    {
+        public static void HarmonicNum(double N)
+        {
+            double HarmonicSum = 0;
+            if (N > 0)
+            {
+                for (double i = 1; i <= N; i++)
+                {
+                    HarmonicSum = HarmonicSum + 1 / i;
+                    
+                }
+
+                Console.WriteLine("the Nth harmonic value is " +HarmonicSum);
+
+            }
+            else
+            {
+                Console.WriteLine(" N should be greter than 0 ");
+            }
+        }
+    }
+}
