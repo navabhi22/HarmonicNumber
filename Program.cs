5575f94442cb7b73efd1739e6fe144f0ffa5e870
navabhi22 <navabhi22@gmail.com>
2022-02-17 18:06:58 +0530

    adding program to get nth sum of harmonic series

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the value of N ");
+            double N = Convert.ToDouble(Console.ReadLine());
+
+            HarmonicNumber.HarmonicNum(N);
+        }
+    }
+}
